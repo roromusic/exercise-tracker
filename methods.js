07080f4733882c1031e3f33d87133909baba918d
Glitch (actually-crayon) <none>
2017-11-08 03:16:50 +0000

    Checkpoint

--- methods.js
+++ methods.js
@@ -15,9 +15,22 @@ methods.addHandler = (req, res, collection) => {
   const body = req.body;
   
   //check if userId (body.userId) is in DB
-  collection.findOne({}, (err, data) => {
+  collection.findOne({id: body.id}, (err, data) => {
     if (err) throw err;
     if(!data) {
+      let obj = {
+        username: body.username
+      };
+      
+      collection.count({}, (err, data) => {
+        if (err) throw err;
+        obj.id = data + 1;
+        
+        collection.insert(Object.assign({log: []}, obj), (err, data) => {
+          if (err) throw err;
+          return res.json(obj);
+        })
+      })
       return res.json({"error": "No such user"})
     }
   })
