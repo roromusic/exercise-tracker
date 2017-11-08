298822a74fceccdb4e5d8b3c1c877944da0d0ce9
Glitch (actually-crayon) <none>
2017-11-08 01:49:50 +0000

    Checkpoint

--- methods.js
+++ methods.js
@@ -3,6 +3,12 @@ const methods = {};
 methods.addHandler = (req, res) => {
   const body = req.body;
   
+  //check if userId (body.userId) is in DB
+  
+  //check if duration is a number
+  
+  //check if date is valid. No data means today. 
+  return res.json(body);
 }
 
 module.exports = methods;
