4fde9c35120bedbd360d9792de647e7e2d6eef05
Glitch (actually-crayon) <none>
2017-11-14 05:59:07 +0000

    Checkpoint

--- methods.js
+++ methods.js
@@ -100,10 +100,20 @@ methods.addHandler = (req, res, collection) => {
 }
 
 methods.getLog = (req, res, collection) => {
-  let userId = req.query.userId;
-  let from = req.query.from;
-  let to = req.query.to;
-  let limit = req.query.limit;
+  let userId = req.query.userId,
+      limit = req.query.limit,
+      from = undefined,
+      to = undefined;
+  
+  let toResults = new chrono.parse(req.query.to);
+  let fromResults = new chrono.parse(req.query.from);
+  
+  if (toResults) to = toResults[0].start.date();
+  if (fromResults) from = fromResults[0].start.date();
+  
+  collection.findOne({})
+  
+
   
   res.end(userId + " " + from + " " + to + " " + limit);
 }
